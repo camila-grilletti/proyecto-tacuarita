e934c885ad090205e7cd1f44b1905e90b91ed4bc
aspeelucas <aspeelucas@gmail.com>
2023-06-17 11:27:42 -0300

    pagina home-corredo

---
+++ js/galeria.js
@@ -0,0 +1,78 @@
+const imagenesArray =  [ {
+    id : `1`,
+    nombre : "chañar",
+    imagen : "./recursos/imagenes/chañaruno.jpg",
+    categoria : "chanar",
+},
+{
+    nombre : "chañar",
+    id : `1`,
+    imagen : "./recursos/imagenes/chañardos.jpg",
+    categoria : "chanar",
+},
+{
+    nombre : "chañar",
+    id : `1`,
+    imagen : "./recursos/imagenes/chañartres.jpg",
+    categoria : "chanar",
+},
+{
+    nombre : "chañar",
+    id : `1`,
+    imagen : "./recursos/imagenes/chañarcuatro.jpg",
+    categoria : "chanar",
+},
+{
+    nombre : "chañar",
+    id : `1`,
+    imagen : "./recursos/imagenes/chañarcinco.jpg",
+    categoria : "chanar",
+},
+{
+    nombre : "chañar",
+    id : `1`,
+    imagen : "./recursos/imagenes/chañarseis.jpg",
+    categoria : "chanar",
+},
+
+{
+    nombre : "durazno de campo",
+    id : `2`,
+    imagen : "./recursos/imagenes/duraznouno.jpg",
+    categoria : "durazno-de-campo",
+},
+{
+    nombre : "durazno de campo",
+    id : `2`,
+    imagen : "./recursos/imagenes/duraznodos.jpg",
+    categoria : "durazno-de-campo",
+},
+{
+    nombre : "durazno de campo",
+    id : `2`,
+    imagen : "./recursos/imagenes/duraznotres.jpg",
+    categoria : "durazno-de-campo",
+},
+{
+    nombre : "durazno de campo",
+    id : `2`,
+    imagen : "./recursos/imagenes/duraznocuatro.jpg",
+    categoria : "durazno-de-campo",
+},
+{
+    nombre : "durazno de campo",
+    id : `2`,
+    imagen : "./recursos/imagenes/duraznocinco.jpg",
+    categoria : "durazno-de-campo",
+},
+{
+    nombre : "durazno de campo",
+    id : `2`,
+    imagen : "./recursos/imagenes/duraznoseis.jpg",
+    categoria : "durazno-de-campo",
+}
+
+
+
+
+] ;        // Array de imagenes        
---
+++ js/index.js
@@ -0,0 +1,104 @@
+
+
+// modal de relevamiento
+
+const btnAbrirModal = document.getElementById('btn-abrir-relevamiento');
+const btnCerrarModal = document.getElementById('btn-cerrar-relevamiento');
+const modalRelenvamiento = document.getElementById('modalAreas');
+
+btnAbrirModal.addEventListener("click",()=>{
+    modalRelenvamiento.showModal();
+});
+
+btnCerrarModal.addEventListener("click",()=>{
+    modalRelenvamiento.close();
+});
+// modal de plantines
+
+const btnAbrirModalPlantines = document.getElementById('btn-abrir-plantines');
+const btnCerrarModalPlantines = document.getElementById('btn-cerrar-plantines');
+const modalPlantines = document.getElementById('modalPlantines');
+
+btnAbrirModalPlantines.addEventListener("click",()=>{
+    modalPlantines.showModal();
+});
+btnCerrarModalPlantines.addEventListener("click",()=>{
+    modalPlantines.close();
+});
+
+// modal recoleccion
+const btnAbrirModalRecoleccion = document.getElementById('btn-abrir-recoleccion');
+const btnCerrarModalRecoleccion = document.getElementById('btn-cerrar-recoleccion');
+const modalRecoleccion = document.getElementById('modalRecoleccion');
+
+btnAbrirModalRecoleccion.addEventListener("click",()=>{
+    modalRecoleccion.showModal();
+});
+
+btnCerrarModalRecoleccion.addEventListener("click",()=>{
+    modalRecoleccion.close();
+});
+
+// modal Plantacion
+
+const btnAbrirModalPlantacion = document.getElementById('btn-abrir-plantacion');
+const btnCerrarModalPlantacion = document.getElementById('btn-cerrar-plantacion');
+const modalPlantacion = document.getElementById('modalPlantacion');
+
+btnAbrirModalPlantacion.addEventListener("click",()=>{
+    modalPlantacion.showModal();
+})
+btnCerrarModalPlantacion.addEventListener("click",()=>{
+    modalPlantacion.close();
+})
+
+
+// GALERIA
+
+const contenedorGaleria = document.querySelector('.contenedorPadreImagenes');
+// const btnCategorias = document.querySelectorAll('.btnCategoria');
+const selectorCategoria = document.getElementById('selectorCategoria');    
+
+function cargarGaleria (especieSeleccionada){
+
+        especieSeleccionada.forEach((imagen)=>{
+        const div = document.createElement('div');
+        div.classList.add('imagenGaleria');
+        div.innerHTML = `
+        <img src="${imagen.imagen}" alt="${imagen.nombre}" class="tamanoImgGaleria">
+        `
+        contenedorGaleria.appendChild(div);
+    })
+
+};  
+
+// cargarGaleria(imagenesArray);
+
+selectorCategoria.addEventListener('change',(e)=>{
+    const arrayFiltrado =  imagenesArray.filter (img => img.id === e.currentTarget.value);
+    contenedorGaleria.innerHTML = "";
+    cargarGaleria(arrayFiltrado);    
+})
+
+// btnCategorias.forEach((btn)=>{
+//     btn.addEventListener('click',(e)=>{
+
+//     const imagenFiltrada =  imagenesArray.filter (img => img.categoria === e.currentTarget.id);
+//     cargarGaleria(imagenFiltrada);    
+
+//     })
+// });
+
+
+// function cargarGaleria (){
+
+//     imagenesArray.forEach((imagen)=>{
+//         const div = document.createElement('div');
+//         div.classList.add('imagenGaleria');
+//         div.innerHTML = `
+//         <img src="${imagen.imagen}" alt="${imagen.nombre}" class="tamanoImgGaleria">
+//         `
+//         contenedorGaleria.appendChild(div);
+//     })
+
+// }; 
